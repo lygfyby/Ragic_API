1d9c3449e1e9d84d9398b1bcfd43dcf8916ed43b
lygfyby <lygfyby@gmail.com>
2023-09-12 16:52:15 +0800

    20230912

---
+++ Ragic_API.cpp
@@ -0,0 +1,140 @@
+#include <Ragic_API.h>
+void RagicAPI::Begin(const char *Authorization)
+{
+    csHTTPClient.setAuthorization(Authorization);
+    csHTTPClient.setTimeout(1500);
+}
+void RagicAPI::Begin(const char *uesr, const char *password)
+{
+    csHTTPClient.setAuthorization(uesr, password);
+    csHTTPClient.setTimeout(1500);
+}
+/**
+ * @brief 修改資料
+ *
+ * @param web 網址列
+ * @param id 資料的ID編號
+ * @param value API內文
+ * @return int 如果成功回傳200
+ */
+int16_t RagicAPI::writeList_Json(JsonObject &injson, POST_Parameters_t &parm)
+{
+    bool testsw = false;
+    int16_t httpCode = NO_connect;
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        String WEB = (parm.web) + (parm.id != 0 ? '/' + (String)parm.id : "");
+        WEB += "?api";
+        WEB += parm.doFormula ? "&doFormula=true" : "";
+        WEB += parm.doDefaultValue ? "&doDefaultValue=true" : "";
+        WEB += parm.doLinkLoad ? "&doLinkLoad=true" : "&doLinkLoad=first";
+        WEB += parm.doWorkflow ? "&doWorkflow=true" : "";
+        WEB += parm.checkLock ? "&checkLock=true" : "";
+        csHTTPClient.begin(WEB);
+        csHTTPClient.addHeader("Content-Type", "application/json");
+        String strPOST = "";
+        serializeJson(injson, strPOST);
+        httpCode = csHTTPClient.POST(strPOST);
+        if (httpCode == HTTP_CODE_OK)
+        {
+            // DynamicJsonDocument returndoc(parm.jsSize);
+            StaticJsonDocument<200> filter;
+            filter["ragicId"] = true;
+            filter["status"] = true;
+            if (testsw)
+            {
+                WiFiClient *stream = csHTTPClient.getStreamPtr();
+                while (stream->available())
+                    Serial.write(stream->read());
+            }
+            DeserializationError error = deserializeJson(*parm.json, csHTTPClient.getStream(), DeserializationOption::Filter(filter));
+            if (error)
+            {
+                _CONSOLE_PRINTF(_PRINT_LEVEL_WARNING, "反序列化失敗:%s\n", error.c_str());
+
+                // return -1;
+            }
+            if ((*parm.json).as<JsonObject>()["status"].as<String>() != "SUCCESS")
+            {
+                if (_CONSOLE_PRINT_LEVEL >= _PRINT_LEVEL_WARNING)
+                {
+                    _CONSOLE_PRINT(_PRINT_LEVEL_WARNING, "寫入失敗!\n");
+                    String strError="";
+                    serializeJson(injson, strError);
+                    _CONSOLE_PRINT(_PRINT_LEVEL_WARNING, strError);
+                    Serial.println(strError);                    
+                    serializeJsonPretty((*parm.json), strError);
+                    _CONSOLE_PRINT(_PRINT_LEVEL_WARNING, strError);
+                    Serial.println(strError);
+                }
+                httpCode = OTHER_ERROR;
+            }
+            else
+            {
+                _CONSOLE_PRINTF(_PRINT_LEVEL_INFO, "OK!ID=%s\n", (*parm.json).as<JsonObject>()["ragicId"].as<String>().c_str());
+            }
+        }
+        else
+            _CONSOLE_PRINT(_PRINT_LEVEL_WARNING, "HTTP ERROR:" + HTTPClient::errorToString(httpCode) + '\n');
+    }
+    csHTTPClient.end();
+    return httpCode;
+}
+int16_t RagicAPI::readList_Json(GET_Parameters_t &parm)
+{
+    bool testsw = false;
+    int16_t httpCode = NO_connect;
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        String WEB = (parm.web) + (parm.id != 0 ? '/' + (String)parm.id : "");
+        WEB += "?api";
+        parm.naming ? WEB += "&naming=FNAME" : WEB += "&naming=EID";
+        if (!parm.subtables)
+            WEB += "&subtables=0";
+        if (parm.listing)
+            WEB += "&listing"; // 是否只讀取列表頁有的資料(""/"coufig")
+        if (parm.info)
+            WEB += "&info"; // 是否連帶讀取建立修改時間、人(""/"coufig")
+        if (parm.approval)
+            WEB += "&approval"; // 是否連帶讀取審核資料(""/"coufig")//?會自動將人名轉換成郵件
+        if (parm.conversation)
+            WEB += "&conversation"; // 是否連帶讀取郵件往來紀錄(""/"coufig")
+        if (parm.commen)
+            WEB += "&commen"; // 是否連帶讀取對話紀錄(""/"coufig")
+        if (parm.ignoreMask)
+            WEB += "&ignoreMask"; // 是否完整顯示密碼欄位(""/"coufig")
+        if (parm.ignoreFixedFilter)
+            WEB += "&ignoreFixedFilter"; // 是否取消固定篩選(""/"coufig")//*需有SYSAdmin權限
+        if (parm.reverse)
+            WEB += "&reverse"; // 反轉資料進來的順序(false=新先舊後/true=舊先新後)//?無效?
+        if (parm.bbcode)
+            WEB += "&bbcode"; // 是否用BBCODE翻譯(false=HTML/true=BBCODE)//?沒測
+        if (parm.other != "")
+            WEB += '&' + parm.other;
+        if (testsw)
+        {
+            _CONSOLE_PRINTF(_PRINT_LEVEL_INFO,"WEB=%s\n", WEB.c_str());
+            DebugInterface.Write(WEB + '\n');
+        }
+        csHTTPClient.begin(WEB);
+        httpCode = csHTTPClient.GET();
+        if (httpCode == HTTP_CODE_OK)
+        {
+            DeserializationError error = deserializeJson(*parm.json, csHTTPClient.getStream());
+            if (error)
+                _CONSOLE_PRINTF(_PRINT_LEVEL_WARNING, "反序列化失敗:%s\n", error.c_str());
+
+            if (testsw)
+            {
+                WiFiClient *stream = csHTTPClient.getStreamPtr();
+                while (stream->available())
+                    Serial.write(stream->read());
+                Serial.println("END");
+            }
+        }
+        else
+            _CONSOLE_PRINT(_PRINT_LEVEL_WARNING, "HTTP ERROR:" + HTTPClient::errorToString(httpCode) + '\n');
+    }
+    csHTTPClient.end();
+    return httpCode;
+}
---
+++ Ragic_API.h
@@ -0,0 +1,59 @@
+#ifndef RAGIC_API_H
+#define RAGIC_API_H
+#include <Template.h>
+class RagicAPI
+{
+private:
+    HTTPClient csHTTPClient;
+    String csAuthorization;
+String csStr;
+public:
+    
+    //String csFrom;
+    //uint32_t csID;
+    //JsonObject csJsojt;
+    //DynamicJsonDocument *csdoc;
+    typedef struct GET_Parameters_t
+    {
+        bool subtables = false;         //是否讀取子表格(0/1)
+        bool naming = false;            //是否用名稱作為key("EID"=數字，"FNAME"=標題名稱);
+        bool listing = false;           //是否只讀取列表頁有的資料(""/"coufig")
+        bool info = false;              //是否連帶讀取建立修改時間、人(""/"coufig")
+        bool approval = false;          //是否連帶讀取審核資料(""/"coufig")//?會自動將人名轉換成郵件
+        bool conversation = false;      //是否連帶讀取郵件往來紀錄(""/"coufig")
+        bool commen = false;            //是否連帶讀取對話紀錄(""/"coufig")
+        bool ignoreMask = false;        //是否完整顯示密碼欄位(""/"coufig")
+        bool ignoreFixedFilter = false; //是否取消固定篩選(""/"coufig")//*需有SYSAdmin權限
+        bool reverse = false;           //反轉資料進來的順序(false=新先舊後/true=舊先新後)//?無效?
+        bool bbcode = false;            //是否用BBCODE翻譯(false=HTML/true=BBCODE)//?沒測
+        String other = "";
+        String web="";
+        uint16_t id=0;
+        DynamicJsonDocument * json=0;
+    } GET_Parameters_t;
+    typedef struct POST_Parameters_t
+    {
+        bool doFormula = true;      //變更/新建前是否先進行公式重算[false/true]//!如果為是則工作流不會執行
+        bool doDefaultValue = true; //變更/新建前是否先載入預設值[false/true]
+        bool doLinkLoad = true;     //變更/新建前是否先計算公式再載入連結[true/first]
+        bool doWorkflow = true;     //是否執行相關的工作流[false/true]
+        bool checkLock = true;      //是否略過上鎖的資料[false/true]
+        String other = "";
+        String web="";
+        uint16_t id=0;
+        DynamicJsonDocument * json=0;
+    } POST_Parameters_t;
+    static const enum ERROR_CODE_e {
+        NO_connect = -100,
+        OTHER_ERROR=-1,
+        SIZE_TOBIG
+    } ERROR_CODE_e;
+    void Begin(const char *Authorization);
+
+    void Begin(const char *uesr, const char *password);
+
+    int16_t writeList_Json(JsonObject& injson,  POST_Parameters_t &parm);
+
+    int16_t readList_Json( GET_Parameters_t &parm);    
+};
+#endif
